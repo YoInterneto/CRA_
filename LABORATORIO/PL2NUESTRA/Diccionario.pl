bc59b5b192c98722962c3f3bd6feaf77c7449fce
JXSXL <48378434+JXSXL@users.noreply.github.com>
2020-04-07 13:53:48 +0200

    Ejercicio analizador separado en varios archivos.

--- LABORATORIO/PL2NUESTRA/Diccionario.pl
+++ LABORATORIO/PL2NUESTRA/Diccionario.pl
@@ -1,14 +1,5 @@
 % Autor:
-% Fecha: 06/04/2020
-
-% Reglas gramaticales
-oracion --> g_nominal, g_verbal.
-g_nominal --> nombre.
-g_nominal --> determinante, nombre.
-g_nominal --> nombre, adjetivo.
-g_verbal --> verbo.
-g_verbal --> verbo, g_nominal.
-g_verbal --> verbo, adjetivo.
+% Fecha: 07/04/2020
 
 %Diccionario
 determinante --> [el].
@@ -44,5 +35,3 @@ preposicion --> [a].
 preposicion --> [en].
 
 pronrelativo --> [que].
-
-
---
+++ LABORATORIO/PL2NUESTRA/Gramatica.pl
@@ -0,0 +1,15 @@
+% Autor:
+% Fecha: 06/04/2020
+
+% Reglas gramaticales
+oracion --> g_nominal, g_verbal.
+g_nominal --> nombre.
+g_nominal --> determinante, nombre.
+g_nominal --> nombre, adjetivo.
+g_verbal --> verbo.
+g_verbal --> verbo, g_nominal.
+g_verbal --> verbo, adjetivo.
+
+
+
+
