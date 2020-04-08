23e0c789833fbf8200c396e86bf6291b2cdf6af0
javi040898 <57791218+javi040898@users.noreply.github.com>
2020-04-08 17:13:13 +0200

    faltan 14 y 10

--- LABORATORIO/PL2NUESTRA/PL2.pl
+++ LABORATORIO/PL2NUESTRA/PL2.pl
@@ -29,39 +29,45 @@
 
 
 
-
+%Oracion compuesta
+compound_sentence(os(O1,C,O2)) -->sentence(O1),conjunction(C),sentence(O2).
 
 
 
 %Oracion simple
-sentence(s(S,V,O)) --> nom_p(S,N,_,_,_), verb(V,N), nom_p(O,_,_,_,_).
+sentence(s(S,V,O)) --> nom_p(S,N,_,_,_,n), verb(V,N), nom_p(O,_,_,_,_,_).
+
+%Caso para coordinada
+sentence(s(V,O)) --> verb(V,_),nom_p(O,_,_,_,_,_).
 
 %Oracion con prep
-sentence(s(S,V,O)) --> nom_p(S,N,_,_,_), verb(V,N), nom_prep(O,_,_,_,_).
+sentence(s(S,V,O)) --> nom_p(S,N,_,_,_,n), verb(V,N), nom_prep(O,_,_,_,_,s).
 
 %Oracion ama a
 %sentence(s(S,V,O)) --> nom_p(S,N,_,_,_), verb(V,N), nom_p(O,_,_,_,_).
 
 
 %Oraciones con sintagmas preposicionales
-sentence(s(S,V,P,O)) --> nom_p(S,N,_,_,_), verb(V,N), preposition(P,E),nom_p(O,_,E,_,_).
+sentence(s(S,V,P,O)) --> nom_p(S,N,_,_,_,n), verb(V,N), preposition(P,E),nom_p(O,_,E,_,_,n).
 %sentence(s(S,V,P,T,O)) --> nom_p(S,N,_,_,_), verb(V,N),nom_p(T,_,_,_,_), preposition(P,E),nom_p(O,_,E,_,_).
 
-sentence(s(S,V,T,O)) --> nom_p(S,N,_,_,_), verb(V,N),nom_p(T,_,_,_,_), nom_prep(O,_,_,_,_).
+sentence(s(S,V,T,O)) --> nom_p(S,N,_,_,_,n), verb(V,N),nom_p(T,_,_,_,_,n), nom_prep(O,_,_,_,_,n).
 
 %Oraciones coordinaads
-sentence(s(S,V,O,C,V2,O2)) --> nom_p(S,N,_,_,_), verb(V,N), nom_p(O,_,_,_,_),conjunction(C),verb(V2,N), nom_p(O2,_,_,_,_).
-sentence(s(S,V,A,C,O,V2,A2)) --> nom_p(S,N,_,T,_), verb(V,N), adjective(A,_,T), conjunction(C),nom_p(O,_,_,T,_),verb(V2,N), adjective(A2,_,T).
+%sentence(s(S,V,O,C,V2,O2)) --> nom_p(S,N,_,_,_,n), verb(V,N), nom_p(O,_,_,_,_,n),conjunction(C),verb(V2,N), nom_p(O2,_,_,_,_,n).
+%sentence(s(S,V,A,C,O,V2,A2)) --> nom_p(S,N,_,T,_,n), verb(V,N), adjective(A,_,T), conjunction(C),nom_p(O,_,_,T,_,n),verb(V2,N), adjective(A2,_,T).
+
+sentence(s(S,V,A)) --> nom_p(S,N,_,T,_,n), verb(V,N), adjective(A,_,T).
 
 %Oracion simple con sintagma adjetival
-sentence(s(S,V,A)) --> nom_p(S,N,_,T,_), verb(V,N),adjective(A,_,T).
+%sentence(s(S,V,A)) --> nom_p(S,N,_,T,_,n), verb(V,N),adjective(A,_,T).
 
 %Oraciones simples con sintagmas adverbiales
-sentence(s(S,V,B)) --> nom_p(S,N,_,_,_), verb(V,N),adverb(B).
-sentence(s(S,V,B,B2)) --> nom_p(S,N,_,_,_), verb(V,N),adverb(B),adverb(B2).
+sentence(s(S,V,B)) --> nom_p(S,N,_,_,_,n), verb(V,N),nom_adv(B).
+%sentence(s(S,V,B)) --> nom_p(S,N,_,_,_,n), verb(V,N),nom_adv(B).
 
 %Oracion subordinada
-sentence(s(S,Y,S2,V,B,V2,O)) --> nom_p(S,N,_,T,_), pronoun(Y),nom_p(S2,_,_,_,_),verb(V,_),adverb(B),verb(V2,N),nom_p(O,_,_,T,_).
+sentence(s(S,Y,S2,V,B,V2,O)) --> nom_p(S,N,_,T,_,n), pronoun(Y),nom_p(S2,_,_,_,_,n),verb(V,_),adverb(B),verb(V2,N),nom_p(O,_,_,T,_,n).
 
 
 %Caso para some apples
@@ -69,13 +75,19 @@ sentence(s(S,Y,S2,V,B,V2,O)) --> nom_p(S,N,_,T,_), pronoun(Y),nom_p(S2,_,_,_,_),
 
 %Caso para some apples
 %nom_p(np(M2,S),N,E,_,_) --> modifier2(M2), noun(S,N,_,E,_,u).
-nom_p(np(S),N,E,_,_) --> modifier2(m2(art2_1)), noun(S,N,_,E,_,u).
+nom_p(np(S),N,E,_,_,n) --> modifier2(m2(art2_1)), noun(S,N,_,E,_,u).
 
-%Para no poner at the universidad, tambien vale para la 7
-nom_p(np(S),N,E,_,C) --> noun(S,N,_,E,l,C).
+%Para no poner at the universidad, tambien vale para la 7 , suponemos que no vale
+nom_p(np(S),N,E,_,C,s) --> noun(S,N,_,E,l,C).
+
+%suponemos que no vale
+%nom_p(np(S),N,E,_,C,s) --> noun(S,N,_,E,l,C).
+
+
 
 %Caso para la 9
-
+%METEMOS UN PARAMETRO NUEVO EN nom_p POR SI LLEVA ALGO ANTES, SI LLEVA(s), NO SE PONE EL THE, SINO SI SE PONE(n)
+nom_p(np(M,S),N,E,T,_,n) --> modifier(M,c), noun(S,N,v,E,T,a).   %caso pregunta 9
 
 
 
@@ -89,14 +101,18 @@ nom_p(np(S),N,E,_,C) --> noun(S,N,_,E,l,C).
 
 
 
-nom_p(np(M,S,A),N,E,T,_) --> modifier(M,L),adjective(A,L,T), noun(S,N,_,E,T,_). %se pone _ al noun porque no se tiene en cuenta la vocal o consonante
-nom_p(np(M,S),N,E,T,_) --> modifier(M,L), noun(S,N,L,E,T,_).
-nom_p(np(S),N,E,T,_) --> noun(S,N,_,E,T,co).
+nom_p(np(M,S,A),N,E,T,_,n) --> modifier(M,L),adjective(A,L,T), noun(S,N,_,E,T,_). %se pone _ al noun porque no se tiene en cuenta la vocal o consonante
+nom_p(np(M,S),N,E,T,co,n) --> modifier(M,L), noun(S,N,L,E,T,co).
+nom_p(np(S),N,E,T,_,n) --> noun(S,N,_,E,T,co).
 %nom_p(np(S),N,E,T,_) --> noun(S,N,_,E,T,a).
-nom_p(np(S,S2),N,E,T,_) --> noun(S,N,_,E,T,_),noun(S2,N,_,E,T,_).
+nom_p(np(S,S2),N,E,T,_,n) --> noun(S,N,_,E,T,_),noun(S2,N,_,E,T,_).
 
 %nom_prep(nprep(P,S),N,E,_,C) --> preposition(P,C), noun(S,N,_,E,_,C).
-nom_prep(nprep(P,O),_,E,_,C) --> preposition(P,E), nom_p(O,_,E,_,C).
+nom_prep(nprep(P,O),_,E,_,C,F) --> preposition(P,E), nom_p(O,_,E,_,C,F).
+
+%Sintagma adverbial
+nom_adv(sadv(B)) --> adverb(B).
+nom_adv(sadv(B,B2)) --> adverb(B),adverb(B2).
 
 %nom_prep(nprep(P,O),_,E,_,C) --> preposition(P,E),nom_p(O,_,E,_,_).
 
@@ -201,42 +217,50 @@ conjunction(c(c_1))--> [and].
 %Hay que annadir persona al verbo pri,seg,ter
 %Annadir cuando es in y cuando at
 
+
+%Oracion compuesta
+oracion_compuesta(os(O1,C,O2)) -->oracion(O1),conjuncion(C),oracion(O2).
+
 %Oracion simple
-oracion(s(S,V,O)) --> sint_n(S,N,_), verbo(V,N,_),sint_n(O,_,e).%probablemente haya que poner lo mismo pero con e2
+oracion(s(S,V,O)) --> sint_n(S,N,_,n), verbo(V,N,_),sint_n(O,_,e,_).%probablemente haya que poner lo mismo pero con e2
+%Caso para coordinada
+oracion(s(V,O)) --> verbo(V,_,_),sint_n(O,_,e,_).
+
 
 %Oracion con prep
-oracion(s(S,V,O)) --> sint_n(S,N,_), verbo(V,N,_),sint_prep(O,_,e).
+oracion(s(S,V,O)) --> sint_n(S,N,_,n), verbo(V,N,_),sint_prep(O,_,e,_).
 
 %Oracion ama a
-oracion(s(S,V,O)) --> sint_n(S,N,_), verbo(V,N,t), preposicion2(p2(p2_1),p), sint_n(O,_,p).
+oracion(s(S,V,O)) --> sint_n(S,N,_,n), verbo(V,N,t), preposicion2(p2(p2_1),p), sint_n(O,_,p,n).
 
 
 %Oracion ama la universidad,
-oracion(s(S,V,O)) --> sint_n(S,N,_), verbo(V,N,t), sint_n(O,_,e2).
+oracion(s(S,V,O)) --> sint_n(S,N,_,n), verbo(V,N,t), sint_n(O,_,e2,s).
 
 %Oraciones con sintagmas preposicionales
 %oracion(s(S,V,P,O)) --> sint_n(S,N,_), verbo(V,N,i), preposicion(P),sint_n(O,_,e).  %cambiar p
 %oracion(s(S,V,P,T,O)) --> sint_n(S,N,_), verbo(V,N,i),sint_n(T,_,_), preposicion(P),sint_n(O,_,_).
 
-oracion(s(S,V,T,O)) --> sint_n(S,N,_), verbo(V,N,i),sint_n(T,_,_), sint_prep(O,_,e).
+oracion(s(S,V,T,O)) --> sint_n(S,N,_,n), verbo(V,N,i),sint_n(T,_,_,n), sint_prep(O,_,e,n).
 
 
 %Oraciones coordinadas
-oracion(s(S,V,O,C,V2,O2)) --> sint_n(S,N,_), verbo(V,N,i),sint_n(O,_,_),conjuncion(C),verbo(V2,N,i),sint_n(O2,_,_).
-oracion(s(S,V,A,C,O,V2,A2)) --> sint_n(S,N,_), verbo(V,N,i),adjetivo(A,_,N),conjuncion(C),sint_n(O,_,_),verbo(V2,N,i),adjetivo(A2,_,N).
+%oracion(s(S,V,O,C,V2,O2)) --> sint_n(S,N,_,n), verbo(V,N,i),sint_n(O,_,_,n),conjuncion(C),verbo(V2,N,i),sint_n(O2,_,_,n).
+oracion(s(S,V,A,C,O,V2,A2)) --> sint_n(S,N,_,n), verbo(V,N,i),adjetivo(A,_,N),conjuncion(C),sint_n(O,_,_,n),verbo(V2,N,i),adjetivo(A2,_,N).
 
 %Oraciones con sintagmas adjetivales
-oracion(s(S,V,A)) --> sint_n(S,N,_), verbo(V,N,i),adjetivo(A,_,N).
+%oracion(s(S,V,A)) --> sint_n(S,N,_,n), verbo(V,N,i),adjetivo(A,_,N).
+oracion(s(S,V,A)) --> sint_n(S,N,p,n), verbo(V,N,i),adjetivo(A,_,N).
 
 %Caso la universidad es grande
-oracion(s(S,V,A)) --> sint_n(S,N,_), verbo(V,N,i),adjetivo(A,_,N).
+oracion(s(S,V,A)) --> sint_n(S,N,e2,n), verbo(V,N,i),adjetivo(A,_,N).
 
 %Oraciones con sintagams adverbiales
-oracion(s(S,V,B)) --> sint_n(S,N,_), verbo(V,N,i),adverbio(B).
-oracion(s(S,V,B,B2)) --> sint_n(S,N,_), verbo(V,N,i),adverbio(B),adverbio(B2).
+oracion(s(S,V,B)) --> sint_n(S,N,_,n), verbo(V,N,i),sint_adv(B).
+%oracion(s(S,V,B)) --> sint_n(S,N,_,n), verbo(V,N,i),sint_adv(B).
 
 %Oracion subordinada
-oracion(s(S,Y,S2,V,B,V2,O)) --> sint_n(S,N,_), pronombre(Y),sint_n(S2,_,_),verbo(V,_,i),adverbio(B),verbo(V2,N,i),sint_n(O,_,_).
+oracion(s(S,Y,S2,V,B,V2,O)) --> sint_n(S,N,_,n), pronombre(Y),sint_n(S2,_,_,n),verbo(V,_,i),adverbio(B),verbo(V2,N,i),sint_n(O,_,_,n).
 
 %Para distinguir cuando hay que poner some o no metemos un parametro de contable (c) o incontable(u)
 %Controlar que no ponga la universidad (cambiando el at)
@@ -247,18 +271,17 @@ oracion(s(S,Y,S2,V,B,V2,O)) --> sint_n(S,N,_), pronombre(Y),sint_n(S2,_,_),verbo
 
 %Para caso de some apples
 %sint_n(np(M2,S),N,_) --> nombre(S,_,N,C,_).
-sint_n(np(S),_,e) --> nombre(S,_,_,u,e).
+sint_n(np(S),_,e,n) --> nombre(S,_,_,u,e).
 
 %PREGUNTA 9
-
-
-
-
 %METEMOS UN PARAMETRO NUEVO EN SINT_N POR SI LLEVA ALGO ANTES, SI LLEVA(s), NO SE PONE EL THE, SINO SI SE PONE(n)
+
+
 %Para caso quitar the university, tambien vale para la 7
 sint_n(np(S),N,e2,s) --> articulo3(m3(art3_1),c2,e2), nombre(S,_,N,_,e2).
  %nom_p(np(S),N,E,_,_) --> modifier2(m2(art2_1)), noun(S,N,_,E,_,u).
  
+sint_n(np(M,S),N,e2,n) --> articulo(M,f,sg,c), nombre(S,_,N,_,e2).   %caso pregunta 9
 
 
 %Para caso quitar the university de ama la universidad
@@ -266,22 +289,25 @@ sint_n(np(S),N,e2,s) --> articulo3(m3(art3_1),c2,e2), nombre(S,_,N,_,e2).
 
 
 
-sint_n(np(M,S,A),N,_,_) --> articulo(M,G,N,C), nombre(S,G,N,C,_), adjetivo(A,G,N).
-sint_n(np(M,S),N,_) --> articulo(M,G,N,C), nombre(S,G,N,C,e).
-sint_n(np(S),N,e) --> nombre(S,_,N,c,e). %solo va a servir para los contables, por eso se mete la c
-sint_n(np(S),N,p) --> nombre(S,_,N,c,p).
+sint_n(np(M,S,A),N,_,n) --> articulo(M,G,N,C), nombre(S,G,N,C,_), adjetivo(A,G,N).
+sint_n(np(M,S),N,_,n) --> articulo(M,G,N,C), nombre(S,G,N,C,e).
+
+sint_n(np(S),N,e,n) --> nombre(S,_,N,c,e). %solo va a servir para los contables, por eso se mete la c
+sint_n(np(S),N,p,n) --> nombre(S,_,N,c,p).
 %sint_n(np(S),N,e2) --> nombre(S,_,N,c2,e2). %solo va a servir para los contables, por eso se mete la c2 ****************************************
-sint_n(np(S,S2),N,_) --> nombre(S,_,N,_,_),nombre(S2,_,N,_,_).
+sint_n(np(S,S2),N,_,n) --> nombre(S,_,N,_,_),nombre(S2,_,N,_,_).
 
  %Sintagma_prep
 %sint_prep(nprep(P,S),N,_) --> preposicion(P), articulo3(m3(art3_1),c2,e2), nombre(S,_,N,C,_).
-sint_prep(nprep(P,O),_,_) --> preposicion(P), sint_n(O,_,e2).
+sint_prep(nprep(P,O),_,_,F) --> preposicion(P), sint_n(O,_,e2,F).
 
 %sint_prep(nprep(P,O),_,_) --> preposicion(P),sint_n(O,_,_).
 
+%Sintagma adverbial
+sint_adv(sadv(B)) --> adverbio(B).
+sint_adv(sadv(B,B2)) --> adverbio(B),adverbio(B2).
 
 
-articulo(m(art_1),f,sg,c) --> [la].
 articulo(m(art_1),f,sg,c) --> [la].
 articulo(m(art_1),m,sg,c) --> [el].
 articulo(m(art_1),f,pl,c) --> [las].
@@ -427,7 +453,7 @@ conjuncion(c(c_1))--> [y].
 %oracion(X,[el,perro,persiguio,un,gato,negro,en,el,jardin],[]),sentence(X,Ingles,[]).
 %sentence(X,[the, dog, chased, a, black, cat, in, the, garden],[]),oracion(X,Espanol,[]).
 
-%Funciona perfectamente         ******************    controlar que ponga el the
+%Perfecta
 %9.  La Universidad es grande. - The University is large.
 %oracion(X,[la, universidad, es, grande],[]),sentence(X,Ingles,[]).
 %sentence(X,[the,university,is,large],[]),oracion(X,Espanol,[]).
@@ -437,20 +463,20 @@ conjuncion(c(c_1))--> [y].
 %oracion(X,[el,hombre,que,vimos,ayer,es,mi,vecino],[]),sentence(X,Ingles,[]).
 %sentence(X,[the,man,that,we,saw,yesterday,is,my,neighbour],[]),oracion(X,Espanol,[]).
 
-%Funciona perfectamente
+%Perfecta
 %11. El canario amarillo canta muy bien. - The yellow canary sings very well.
 %oracion(X,[el,canario,amarillo,canta,muy,bien],[]),sentence(X,Ingles,[]).
 %sentence(X,[the, yellow, canary, sings, very, well],[]),oracion(X,Espanol,[]).
 
-%Funciona perfectamente
+%Perfecta
 %12.  Juan toma un cafe y lee el periodico. - John has a co?ee and reads the newspaper.
-%oracion(X,[juan,toma,un,cafe,y,lee,el,periodico],[]),sentence(X,Ingles,[]).
-%sentence(X,[john, has, a, coffee, and, reads, the, newspaper],[]),oracion(X,Espanol,[]).
+%oracion_compuesta(X,[juan,toma,un,cafe,y,lee,el,periodico],[]),compound_sentence(X,Ingles,[]).
+%compound_sentence(X,[john, has, a, coffee, and, reads, the, newspaper],[]),oracion_compuesta(X,Espanol,[]).
 
 %Funciona perfectamente
 %13. Juan es delgado y Maria es alta. - John is thin and Mary is tall.
-% oracion(X,[juan,es,delgado,y,maria,es,alta],[]),sentence(X,Ingles,[]).
-%sentence(X,[john, is,thin,and,mary,is,tall],[]),oracion(X,Espanol,[]).
+%oracion_compuesta(X,[juan,es,delgado,y,maria,es,alta],[]),compound_sentence(X,Ingles,[]).
+%compound_sentence(X,[john, is,thin,and,mary,is,tall],[]),oracion_compuesta(X,Espanol,[]).
 
 %Falta comprobar quitar el de
 %14. Oscar Wilde escribio El Fantasma de Canterville - Oscar Wilde wrote The Canterville Ghost.
